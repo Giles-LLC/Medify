1b477761e6871cb459b4377d6a00ebb24ac345e2
Micheal <mikegg21@gmail.com>
2025-01-01 16:27:29 -0600

    downloaded template for quick start

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,28 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatToolbarModule,MatFormFieldModule,MatInputModule,MatCardModule],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Medify';
-
-  onLogin(): void {
-    console.log('Login button clicked');
-    // Add logic for navigating to the login page or handling authentication
-    // Example: this.router.navigate(['/login']);
-  }
-
-  onSignup(): void {
-    console.log('Sign Up button clicked');
-    // Add logic for navigating to the sign-up page or handling user registration
-    // Example: this.router.navigate(['/signup']);
-  }
+  title = 'Modernize Angular Admin Tempplate';
 }
